ae82a1a78536988483568b96f75f5613684de8e3
eirinits <164929577+eirinitsironi@users.noreply.github.com>
2025-04-15 13:54:55 +0300

    Quiz

--- src/com/artventure/Quiz.java
+++ src/com/artventure/Quiz.java
@@ -15,4 +15,45 @@ public class Quiz {
         this.recommendedMuseums = new ArrayList<>();    //ready empty list for the recommended museums
     }
 
+    public void generateQuiz() {
+        System.out.println("Welcome to the Preferences Quiz!");
+        Scanner scanner = new Scanner(System.in);   //with GUI we need buttons instead 
+       
+        for (Question q : questions) {
+            System.out.println(q.getText());
+            List<String> options = q.getOptions();
+            
+            for (int i = 0; i < options.size(); i++) {
+                System.out.println((i + 1) + ". " + options.get(i));
+            }
+
+            System.out.print("Choice 1-4: ");
+            int choice = scanner.nextInt();
+            if (choice >= 1 && choice <= 4) {
+                recordAnswer(options.get(choice -1)); 
+            }
+            else {
+                System.out.println("Invalid choice. Quitting Quiz.");
+                saveProgress();
+                return;
+            }
+            scanner.close();
+        }
+    }
+
+    private void recordAnswer(String answer) {
+        answers.add(answer);
+    }
+
+    public void saveProgress() {
+
+    }
+
+    public void analyzePreferences() {
+
+    }
+
+    public void generateRecommendations() {
+
+    }
 }

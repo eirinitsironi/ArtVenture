c9fe6ff2edc849e09f32426ee2bb70447218547a
valiaflorou <vslflorou@gmail.com>
2025-05-05 11:22:24 +0300

    SearchDetails

---
+++ src/com/artventure/SearchResult.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class SearchResult {
+    private int id;
+
+    public SearchResult(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void viewDetails(Object r) {
+        System.out.println("Παραπάνω λεπτομέρειες" + id);
+    }
+}

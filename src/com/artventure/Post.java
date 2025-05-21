e67f2c7cd8318d76870ae867fe5ead0d624b0821
valiaflorou <vslflorou@gmail.com>
2025-05-21 18:45:33 +0300

    Review

--- src/com/artventure/Post.java
+++ src/com/artventure/Post.java
@@ -1,12 +1,18 @@
 import java.time.LocalDateTime;
 
 public abstract class Post {
+    protected String title;
     protected String category;
     protected LocalDateTime createdAt;
 
-    public Post(String category) {
+    public Post(String title, String category) {
+        this.title = title;
         this.category = category;
         this.createdAt = LocalDateTime.now();
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public abstract void preview();
